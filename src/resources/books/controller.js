865ffc414c55a58f779f643cf58c62546b6db72c
Viktorija Z <25870341+vzaboraite@users.noreply.github.com>
2021-10-07 22:47:28 +0100

    Build `getOneById` controller for `Books` resource.

--- src/resources/books/controller.js
+++ src/resources/books/controller.js
@@ -50,7 +50,28 @@ const getAll = async (req, res) => {
   }
 };
 
+const getOneById = async (req, res) => {
+  const { id } = req.params;
+
+  const getOneByIdSql = `
+  SELECT *
+  FROM books
+  WHERE id = $1
+  `;
+
+  try {
+    const result = await db.query(getOneByIdSql, [req.params.id]);
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error("[ERROR] getOneById: ", { error: error.message });
+
+    res.status(500).json({ error: error.message });
+  }
+};
+
 module.exports = {
   createOne,
   getAll,
+  getOneById,
 };
